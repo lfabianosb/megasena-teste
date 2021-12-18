2b016a0e6a22f08098822de76ee0f1bcb318cbaf
Fabiano Bandeira <lfabianosb@gmail.com>
2021-12-18 17:08:58 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import fetch from "node-fetch";
+
+const options = {
+  method: 'GET',
+  headers: {
+    'Cookie': 'security=true'
+  },
+};
+
+fetch('http://loterias.caixa.gov.br/wps/portal/loterias/landing/megasena/!ut/p/a1/04_Sj9CPykssy0xPLMnMz0vMAfGjzOLNDH0MPAzcDbwMPI0sDBxNXAOMwrzCjA0sjIEKIoEKnN0dPUzMfQwMDEwsjAw8XZw8XMwtfQ0MPM2I02-AAzgaENIfrh-FqsQ9wNnUwNHfxcnSwBgIDUyhCvA5EawAjxsKckMjDDI9FQE-F4ca/dl5/d5/L2dBISEvZ0FBIS9nQSEh/pw/Z7_HGK818G0KO6H80AU71KG7J0072/res/id=buscaResultado/c=cacheLevelPage//p=concurso=2437', options)
+    .then(res => res.json())
+    .then(json => console.log(json))
+    .catch(err => console.log('error', err));
