ef30b06e06822e53f886fc62481344b32a704023
Fabiano Bandeira <lfabianosb@gmail.com>
2021-12-18 17:13:50 -0300

    text

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ const options = {
 };
 
 fetch('http://loterias.caixa.gov.br/wps/portal/loterias/landing/megasena/!ut/p/a1/04_Sj9CPykssy0xPLMnMz0vMAfGjzOLNDH0MPAzcDbwMPI0sDBxNXAOMwrzCjA0sjIEKIoEKnN0dPUzMfQwMDEwsjAw8XZw8XMwtfQ0MPM2I02-AAzgaENIfrh-FqsQ9wNnUwNHfxcnSwBgIDUyhCvA5EawAjxsKckMjDDI9FQE-F4ca/dl5/d5/L2dBISEvZ0FBIS9nQSEh/pw/Z7_HGK818G0KO6H80AU71KG7J0072/res/id=buscaResultado/c=cacheLevelPage//p=concurso=2437', options)
-    .then(res => res.json())
-    .then(json => console.log(json))
+    .then(res => res.text())
+    .then(text => console.log(text))
     .catch(err => console.log('error', err));
